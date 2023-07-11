8f3e4acfaa0190b27c472f199dd0a6fa4db5a5a7
Suhayl Ahmed <suhayl.ahmed@icloud.com>
2023-07-11 12:01:27 +0100

    Add project

---
+++ InputFieldTests/InputFieldTests/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  InputFieldTests
+//
+//  Created by Suhayl Ahmed on 11/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ InputFieldTests/InputFieldTests/InputFieldTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputFieldTestsApp.swift
+//  InputFieldTests
+//
+//  Created by Suhayl Ahmed on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct InputFieldTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
